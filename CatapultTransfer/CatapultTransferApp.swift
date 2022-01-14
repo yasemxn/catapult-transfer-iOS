38da52c78a722eb6aee1ac58cdf0cc1af609d3dc
Emin Deniz <emndeniz@gmail.com>
2022-01-14 14:26:00 +0300

    Initial Commit

---
+++ CatapultTransfer/CatapultTransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatapultTransferApp.swift
+//  CatapultTransfer
+//
+//  Created by Mehmet Emin Deniz on 14.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatapultTransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatapultTransfer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatapultTransfer
+//
+//  Created by Mehmet Emin Deniz on 14.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
